3705b0ec2d1ba348e24cb3b5a0b539f9a749e02a
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-24 00:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function volume_sphere() {
+function getOutput(){
+	let rad = document.getElementById("radius").value
+  let volSphere = volume_sphere(rad)
+  document.getElementById("volume").value = volSphere
+}
+
+function volume_sphere(r) {
     //Write your code here
-  
+	let vol = (4/3)*3.14*r*r*r
+	return vol
 } 
+/* console.log(volume_sphere(3)) */
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
